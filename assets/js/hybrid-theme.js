0951ee70192bb8e093dd6b872eb970fc183f7220
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-02 21:35:44 -0500

    add: paragraph block variation

---
+++ assets/js/hybrid-theme.js
@@ -0,0 +1,10 @@
+wp.blocks.registerBlockVariation('core/paragraph', {
+  name: 'intro-paragraph',
+  title: 'Intro Paragraph',
+  description: 'A paragraph with large text and a drop cap.',
+  attributes: {
+      fontSize: 'medium',
+      dropCap: true,
+      placeholder: 'Write an introduction.',
+  },
+});
