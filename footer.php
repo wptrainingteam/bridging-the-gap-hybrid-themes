7d709df55cfda16f7a6c87e9edc17a17c3246b1c
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-02 22:02:05 -0500

    add: footer block template

--- footer.php
+++ footer.php
@@ -11,21 +11,7 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hybrid-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hybrid-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hybrid-theme' ), 'hybrid-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<?php block_template_part( 'footer' ); ?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- functions-hybrid.php
+++ functions-hybrid.php
@@ -22,3 +22,9 @@ function hybrid_theme_enqueue_styles() {
   add_editor_style( 'assets/css/hybrid-theme.css' );
 }
 add_action( 'enqueue_block_assets', 'hybrid_theme_enqueue_styles' );
+
+// Add support for block template parts
+function hybrid_add_template_parts_support() {
+  add_theme_support( 'block-template-parts' );
+}
+add_action( 'after_setup_theme', 'hybrid_add_template_parts_support' );
